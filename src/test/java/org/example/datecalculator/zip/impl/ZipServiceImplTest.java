e21ac999406a1d5080c3a7b94548e9b5fdd7e5a2
chuck hutchinson <chuckhutchinson2@yahoo.com>
2023-04-24 14:45:14 -0400

    updates

--- src/test/java/org/example/datecalculator/zip/impl/ZipServiceImplTest.java
+++ src/test/java/org/example/datecalculator/zip/impl/ZipServiceImplTest.java
@@ -1,13 +1,11 @@
 package org.example.datecalculator.zip.impl;
 
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
@@ -18,17 +16,11 @@ public class ZipServiceImplTest {
 
     ZipServiceImpl zipService;
 
-    @Before
-    public void setup () throws FileNotFoundException {
-
+    @Test
+    public void testZipDirectory() throws IOException {
         FileOutputStream fileOutputStream = new FileOutputStream("images.zip");
 
         zipService = new ZipServiceImpl(fileOutputStream);
-    }
-
-    @Test
-    public void testZip() throws IOException {
         zipService.archive("images", Arrays.asList("*.jpg"));
     }
-
 }
